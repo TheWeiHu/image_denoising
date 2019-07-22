6962bf841355ec2c05ee0649860d36cb53424d02
TheWeiHu <wei@weihu.ca>
2019-07-22 16:36:51 +0800

    new model - complete

--- cnn.py
+++ cnn.py
@@ -5,20 +5,7 @@ Some description
 import os
 import pickle
 import random
-import tensorflow as tf
-import psnr
-
-
-def psnr(im1, im2):
-    """
-    https://github.com/XiaoCode-er/python-PSNR/blob/master/psnr.py
-    """
-    mse = tf.reduce_mean(tf.squared_difference(im1, im2))
-    result = (
-        tf.constant(255 ** 2, dtype=tf.float32) / mse
-    )  # TODO: Maybe fix pixel scale later?
-    result = tf.math.multiply(tf.constant(10, dtype=tf.float32), log10(result))
-    return result.eval()
+import tensorflow as tfx
 
 
 def save_loss_array(table, filename="model/loss_array"):
@@ -46,6 +33,24 @@ def load_loss_array(filename="model/loss_array"):
     new_dict = pickle.load(infile)
     infile.close()
     return new_dict
+
+
+def log10(x):
+    numerator = tf.log(x)
+    denominator = tf.log(tf.constant(10, dtype=numerator.dtype))
+    return numerator / denominator
+
+
+def psnr(im1, im2):
+    """
+    https://github.com/XiaoCode-er/python-PSNR/blob/master/psnr.py
+    """
+    mse = tf.reduce_mean(tf.squared_difference(im1, im2))
+    result = (
+        tf.constant(255 ** 2, dtype=tf.float32) / mse
+    )  # TODO: Maybe fix pixel scale later?
+    result = tf.math.multiply(tf.constant(10, dtype=tf.float32), log10(result))
+    return result.eval()
 
 
 def generate_file_list(file_path):
--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-    # # TODO: determine if the cast is optional
-    # flattened_output = tf.cast(flattened_output, tf.float32)
-    # flattened_noise = tf.cast(flattened_noise, tf.float32)
